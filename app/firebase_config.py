3fc6fb369382dc337da4d4ea56897f7f9135968e
ValereCode <setodji2001@gmail.com>
2024-09-07 23:27:16 +0000

    json file

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -4,5 +4,6 @@ from .config import settings
 import json
 
 cred = credentials.Certificate(json.loads(settings.google_application_credentials))
+#cred = credentials.Certificate(settings.google_application_credentials)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
